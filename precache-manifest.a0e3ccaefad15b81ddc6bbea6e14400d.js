b6c54de6724114a06126fa56b795e3ef4b51def3
ma'rifat ma'ruf <riddbengkok@gmail.com>
2019-07-02 10:54:15 +0700

    Updates

---
+++ precache-manifest.a0e3ccaefad15b81ddc6bbea6e14400d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d019193e50e7dd21c359e2db8147e883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75379f4fcac01405f4bf",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a97f55e7004d0797ee6a",
+    "url": "/static/js/2.67d3ae61.chunk.js"
+  },
+  {
+    "revision": "75379f4fcac01405f4bf",
+    "url": "/static/js/main.7e5f2b3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
